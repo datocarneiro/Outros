a9fbe2882a1762ed6c6a2421d9dc82a301ec140e
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-07-08 15:57:16 -0300

    a

--- Automação_Web/AutoWeb.py
+++ Automação_Web/AutoWeb.py
@@ -7,31 +7,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
 
 servico = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=servico)
+navegador = webdriver.Chrome(service=servico)
 
-driver.get("https://amplo.eship.com.br/")
-driver.find_element(By.XPATH, '//*[@id="login"]').send_keys("dashboard3")
-driver.find_element(By.XPATH, '//*[@id="senha"]').send_keys("12341234")
-driver.find_element(By.XPATH, '//*[@id="Entrar"]/span').click()
+navegador.get("https://amplo.eship.com.br/")
+navegador.find_element(By.XPATH, '//*[@id="login"]').send_keys("dashboard3")
+navegador.find_element(By.XPATH, '//*[@id="senha"]').send_keys("12341234")
+navegador.find_element(By.XPATH, '//*[@id="Entrar"]/span').click()
 
 # Aguardar o carregamento da página
 time.sleep(5)
 
 # Selecionar a codificação em ISO-8859-1
-driver.find_element(By.XPATH, '//*[@id="formulario"]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/label').click()
+navegador.find_element(By.XPATH, '//*[@id="formulario"]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/label').click()
 
 # Marcar os campos para exportação
-driver.find_element(By.XPATH, '//*[@id="formulario"]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div/div[2]/div[6]/label').click()
+navegador.find_element(By.XPATH, '//*[@id="formulario"]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div/div[2]/div[6]/label').click()
 
 # Clicar no botão exportar
-driver.find_element(By.XPATH, '//*[@id="cFuncaoListarRemessas_Exportar"]/span').click()
-
+navegador.find_element(By.XPATH, '//*[@id="cFuncaoListarRemessas_Exportar"]/span').click()
 
 # Aguardar o download ser concluído
 time.sleep(30)
 
 # Fechar o navegador
-#driver.quit()
+#navegador.quit()
 print(input("deseja sair:(s/n)"))
 
 # Executar a função de download do relatório a cada 30 minutos
