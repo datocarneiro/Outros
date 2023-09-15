86e5d079ac2cc3cbb05adfed8f16449cb78d8725
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-09-15 15:34:10 -0300

    a

--- Entradas x Saidas/PythonExportarBonitoPlanilha.py
+++ Entradas x Saidas/PythonExportarBonitoPlanilha.py
@@ -1,16 +1,23 @@
+'''
+O código lida com a leitura de um arquivo CSV, manipula os dados,
+cria um novo arquivo CSV com os dados formatados corretamente,
+realiza operações com o pandas para análise dos dados 
+e salva o resultado em outro arquivo CSV.
+'''
+
 import pandas as pd
 import csv
 import os
 
+# Define os caminhos dos arquivos de entrada e saída, usando as variáveis 'diretorioPlanilha, nome_arquivo e nomeSalvar.
 diretorioPlanilha = 'D:\Repositórios GitHub\AutomacaoNubank\chargeData.csv'
 nome_arquivo = 'D:\Repositórios GitHub\AutomacaoNubank\ArquivoTemporario.csv'
 nomeSalvar = 'D:\Repositórios GitHub\AutomacaoNubank\\resultado.csv'
-# diretorioPlanilha = 'chargeData.csv'
-# nome_arquivo = 'ArquivoTemporario.csv'
-# nomeSalvar = 'resultado.csv'
 
+# Inicializa uma lista chamada dados com o cabeçalho das colunas: ['Data', 'Descricao', 'Gastos'].
 dados = [['Data','Descricao','Gastos']]
 
+# Define a função obter_numero_do_mes(mes) que recebe um mês abreviado como entrada e retorna o número do mês correspondente.
 def obter_numero_do_mes(mes):
     meses = {
         'Jan': 1,
@@ -28,54 +35,64 @@ def obter_numero_do_mes(mes):
     }
     return meses.get(mes, None)
 
+# Abre o arquivo CSV especificado em diretorioPlanilha para leitura 
+# (mode='r') e cria um objeto leitor_csv para ler o conteúdo do arquivo no formato de dicionário.
 with open(diretorioPlanilha, mode='r') as arquivo_csv:
     leitor_csv = csv.DictReader(arquivo_csv)
     
-
+    # Itera sobre cada linha do arquivo CSV utilizando um loop for.
     for linha in leitor_csv:
+        #Extrai o ano da coluna 'Ano'.
         ano = linha['Ano']
+        #Divide a coluna 'date' em partes separadas por espaço 
+        # e pega apenas a segunda parte (representando o mês).
         date = linha['date'].split(' ')[1]
+        #Substitui os caracteres '.' por vazio e ',' por '.' na coluna 'amount' 
+        # e converte o resultado para um número de ponto flutuante.
         amount = float(linha['amount'].replace('.','').replace(',','.'))
-
+        #Se o valor for negativo, define-o como zero.
         if(amount < 0):
             amount = 0
-
+        # Formata a data no formato 'dd/mm/aaaa' e adiciona à lista dados a data, descrição e gasto da linha atual.
         data = linha['date'].split(' ')[0] + '/' + str(obter_numero_do_mes(linha['date'].split(' ')[1])) + '/' + ano
         dados.append([data, linha['description'], amount])
 
 
-    # Abra o arquivo CSV em modo de escrita
+    # Abre o arquivo nome_arquivo para escrita (mode='w') 
     with open(nome_arquivo, mode='w', newline='') as arquivo_csv:
-        # Crie um objeto escritor CSV
+        # cria um objeto escritor_csv para escrever os dados no formato CSV.
         escritor_csv = csv.writer(arquivo_csv)
         
-        # Escreva os dados no arquivo CSV
+        # Itera sobre cada linha em dados e escreve-a no arquivo CSV.
         for linha in dados:
             escritor_csv.writerow(linha)
 
+    #Imprime uma mensagem informando que o arquivo CSV foi criado com sucesso.
     print(f'O arquivo CSV "{nome_arquivo}" foi criado com sucesso.')
 
-# Ler a planilha para um DataFrame (substitua 'seuarquivo.csv' pelo caminho do seu arquivo CSV)
+# Lê o arquivo CSV recém-criado em um DataFrame usando a biblioteca pandas.
 df = pd.read_csv(nome_arquivo)
 
-# Certifique-se de que a coluna 'Data' está no formato de data
+# Converte a coluna 'Data' para formato de data usando pd.to_datetime.
 df['Data'] = pd.to_datetime(df['Data'])
 
-# Criar colunas separadas para o mês e o ano
+# Cria duas novas colunas no DataFrame, 'Ano' e 'Mes',
+# que correspondem ao ano e mês extraídos da coluna 'Data'.
 df['Ano'] = df['Data'].dt.year
 df['Mes'] = df['Data'].dt.month
 
-# Agrupar os dados por mês e ano e somar os gastos
+# Agrupa os dados por ano e mês e soma os gastos.
 resultado = df.groupby(['Ano', 'Mes'])['Gastos'].sum().reset_index()
 
+#Se o arquivo nome_arquivo existir, ele é removido.
 if os.path.exists(nome_arquivo):
-    # Exclua o arquivo
     os.remove(nome_arquivo)
+    # Imprime uma mensagem informando que o arquivo CSV foi excluído com sucesso.
     print(f'O arquivo CSV "{nome_arquivo}" foi excluido com sucesso.')
 
-# Exibir o resultado
-# print(resultado)
+# Salva o resultado no arquivo nomeSalvar no formato CSV, sem incluir o índice.
 resultado.to_csv(nomeSalvar, index=False)
+# Limpa a tela do console.
 os.system('cls')
 print('-'*100)
 print(f'O arquivo CSV "{nomeSalvar}" foi Criado com sucesso.')
---
+++ Entradas x Saidas/readme.md
@@ -0,0 +1,5 @@
+
+O código lida com a leitura de um arquivo CSV, manipula os dados,
+cria um novo arquivo CSV com os dados formatados corretamente,
+realiza operações com o pandas para análise dos dados 
+e salva o resultado em outro arquivo CSV.
