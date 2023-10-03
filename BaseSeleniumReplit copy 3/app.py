310cbaf2f843424fbeb3762b77acdbccb1421d71
datocarneiro <datocarneiro@outlook.com>
2023-10-03 00:45:56 -0300

    a

--- BaseSeleniumReplit copy 3/app.py
+++ BaseSeleniumReplit copy 3/app.py
@@ -1,9 +1,7 @@
 import time
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 from openpyxl import load_workbook
 from werkzeug.utils import secure_filename
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from pathlib import Path
+import xlsxwriter
+import os
 
 options = Options()
 options.add_argument("--no-sandbox")
@@ -23,50 +24,10 @@ app = Flask(__name__)
 
 lista_pendentes = [] # Variável global para armazenar a lista
 
-@app.route('/')
-def index():
-    return render_template('index.html', pendentes=lista_pendentes)
-
-@app.route('/', methods=['POST'])
-def preparar_dados_planilha():
-    global lista_pendentes # Acessando a variável global
-    file = request.files['file']
-    
-    # Solicitar ao usuário que escolha o nome de saída para o arquivo DataFrame
-    # arquivo_saida = secure_filename(file.filename).replace('.xlsx', '_modificado.xlsx')
-
-    if not file.filename.endswith('.xlsx'):
-        return "Por favor, selecione um arquivo Excel (.xlsx)"
-
-    # Carregar planilha
-    planilha = load_workbook(file)
-    aba_ativa = planilha.active
-
-    # LER A PLANILHA, E CRIAR UMA LISTA SOMENTE COM AS ENTREGAS DIFERENTE DE "ENTREGUE"
-    lista_pendentes = []
-    for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:], aba_ativa["C"][1:], aba_ativa["D"][1:]):
-        # se os dados da coluna "D(status)" forem diferente de "Entregue", adicione(append) à lista os dados da coluna "C(AWB)".
-        if coluna_d.value != 'ENTREGUE':
-            if coluna_c.value is not None:
-                lista_pendentes.append(coluna_c.value)
-
-    print("="*150)
-    print(f'As pendente de entrega são: {lista_pendentes}')
-    print("="*150)
-
-    statuses, datas = capturar_status_pendentes()
-    capturas = []  # Lista para armazenar os valores de captura
-
-    for awb, status, data in zip(lista_pendentes, statuses, datas):
-        captura = f'dados capturados: | AWB: {awb} | STATUS:{status} | DATA_EVENTO: {data} |'
-        capturas.append(captura)  # Adicionar a captura à lista
-    
-    return render_template('index.html', pendentes=lista_pendentes, statuses=statuses, datas=datas, capturas=capturas)
-
 # FUNÇÃO PARA CONSULTAR STATUS DE RASTREAMENTO NO SITE DA LATAM
 def captura_status(awb):
     driver.get(f"https://www.latamcargo.com/en/trackshipment?docNumber={awb}&docPrefix=957&soType=SO")
-    
+
     wait = WebDriverWait(driver, 10)
 
     status_evento = wait.until(
@@ -79,23 +40,59 @@ def captura_status(awb):
 
     captura = f'dados capturados: | AWB: {awb} | STATUS:{status} | DATA_EVENTO: {data} |'
     print(captura)
-    
-    return status, data
+    print("="*150)
+
+    return status, data, captura
 
 def capturar_status_pendentes():
     dados_rastreamento = []
     statuses = []  # Lista para armazenar os valores de status
     datas = []  # Lista para armazenar os valores de data
-    
+
     for awb in lista_pendentes:
-        status, data = captura_status(awb)
+        status, data, _ = captura_status(awb)  # Descartar o valor da captura
         dados_rastreamento.append({'AWB': awb, 'STATUS': status, 'DATA_EVENTO': data})
         statuses.append(status)  # Adicionar o status à lista
         datas.append(data)  # Adicionar a data à lista
+
+    df_rastreamento = pd.DataFrame(dados_rastreamento)
+    print(df_rastreamento)
+
+    return df_rastreamento
+
+@app.route('/', methods=['POST'])
+def preparar_dados_planilha():
+    global lista_pendentes # Acessando a variável global
+    file = request.files['file']
     
-    # df_rastreamento = pd.DataFrame(dados_rastreamento)
-    
-    return statuses, datas
+    if not file.filename.endswith('.xlsx'):
+        return "Por favor, selecione um arquivo Excel (.xlsx)"
+
+    # Carregar planilha
+    planilha = load_workbook(file)
+    aba_ativa = planilha.active
+
+    # LER A PLANILHA, E CRIAR UMA LISTA SOMENTE COM AS ENTREGAS DIFERENTE DE "ENTREGUE"
+    lista_pendentes = [coluna_c.value for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:], aba_ativa["C"][1:], aba_ativa["D"][1:]) if coluna_d.value != 'ENTREGUE' and coluna_c.value is not None]
+
+    print("=" * 150)
+    print(f'As pendente de entrega são: {lista_pendentes}')
+    print("=" * 150)
+
+    df_rastreamento = capturar_status_pendentes()
+    statuses = df_rastreamento['STATUS']
+    datas = df_rastreamento['DATA_EVENTO']
+
+    # Salvar o DataFrame em um arquivo Excel na pasta "Downloads"
+    nome_arquivo = 'rastreamento.xlsx'
+    caminho_arquivo = os.path.join(os.path.expanduser("~"), "C:/Downloads", nome_arquivo)
+    df_rastreamento.to_excel(caminho_arquivo, index=False)
+
+    return render_template('index.html', pendentes=lista_pendentes, statuses=statuses, datas=datas)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
--- Rast_Latam(editar)/app.py
+++ Rast_Latam(editar)/app.py
@@ -1,31 +1,35 @@
-from openpyxl import load_workbook
-import os
+import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from flask import Flask, render_template, jsonify, request
+from openpyxl import load_workbook
+from werkzeug.utils import secure_filename
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import pandas as pd
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
-import pathlib
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.headless = False  # Executar o Chrome de forma oculta
+driver = webdriver.Chrome(options=options) # replit
 
+# Inicialização do aplicativo Flask
 app = Flask(__name__)
+
+lista_pendentes = [] # Variável global para armazenar a lista
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pendentes=lista_pendentes)
 
 @app.route('/', methods=['POST'])
-def process_form():
+def preparar_dados_planilha():
+    global lista_pendentes # Acessando a variável global
     file = request.files['file']
     
-    # Solicitar ao usuário que escolha o nome de saída para o arquivo DataFrame
-    arquivo_saida = secure_filename(file.filename).replace('.xlsx', '_modificado.xlsx')
-
     if not file.filename.endswith('.xlsx'):
         return "Por favor, selecione um arquivo Excel (.xlsx)"
 
@@ -34,72 +38,52 @@ def process_form():
     aba_ativa = planilha.active
 
     # LER A PLANILHA, E CRIAR UMA LISTA SOMENTE COM AS ENTREGAS DIFERENTE DE "ENTREGUE"
-    lista = []
+    lista_pendentes = [coluna_c.value for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:], aba_ativa["C"][1:], aba_ativa["D"][1:]) if coluna_d.value != 'ENTREGUE' and coluna_c.value is not None]
 
-    for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:], aba_ativa["C"][1:], aba_ativa["D"][1:]):
-        # se os dados da coluna "D(status)" forem diferente de "Entregue", adicione(append) à lista os dados da coluna "C(AWB)".
-        if coluna_d.value != 'ENTREGUE':
-            if coluna_c.value is not None:
-                lista.append(coluna_c.value)
+    print("=" * 150)
+    print(f'As pendente de entrega são: {lista_pendentes}')
+    print("=" * 150)
 
-    print("="*150)
-    print(f'As pendente de entrega são: {lista}')
-    print("="*150)
+    df_rastreamento = capturar_status_pendentes()
 
-    servico = Service(ChromeDriverManager().install())
+    return render_template('index.html', pendentes=lista_pendentes, statuses=df_rastreamento['STATUS'], datas=df_rastreamento['DATA_EVENTO'], capturas=df_rastreamento['CAPTURAS'])
 
-    # ABRI NAVEGADOR
-    opcoes = Options()
-    opcoes.headless = False  # modo off ou não
-    driver = webdriver.Chrome(service=servico, options=opcoes)
-
-    tabela = {}
-
-    # FUNÇÃO PARA CONSULTAR STATUS DE RASTREAMENTO NO SITE DA LATAM
-    def captura_status(awb):
-        driver.get(f"https://www.latamcargo.com/en/trackshipment?docNumber={awb}&docPrefix=957&soType=SO")
-        
-        # Aguarde até que o elemento esteja visível
-        wait = WebDriverWait(driver, 10)
-
-        status_evento = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[1]')))
-        status = status_evento.text 
-
-        data_evento = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[6]')))
-        data = data_evento.text 
-
-        print(f'dados capturados: | AWB: {awb} | STATUS:{status} | DATA_EVENTO: {data} |')
-      
-        return [status, data]
-        
-    dados_rastreamento = []
-    for awb in lista:
-        status, data = captura_status(awb)
-        dados_rastreamento.append({'AWB': awb, 'STATUS': status, 'DATA_EVENTO': data})
-
-    df_rastreamento = pd.DataFrame(dados_rastreamento)
-    print(df_rastreamento)
-
-  
-    # Obter o diretório da pasta de downloads do sistema operacional
-    diretorio_downloads = str(pathlib.Path.home() / "Downloads")
-
-    # Caminho completo para o arquivo de saída na pasta de downloads
-    caminho_saida = os.path.join(diretorio_downloads, arquivo_saida)
-
-    # Salvar o DataFrame modificado em um arquivo Excel na pasta de downloads
-    df_rastreamento.to_excel(caminho_saida, index=False)
-
-
-    print(f"Arquivo Excel '{arquivo_saida}' criado com sucesso.")
-
-    # return "Consulta de rastreamento realizada com sucesso!"
-    # Renderizar a página HTML e passar a variável resultado como argumento
+# FUNÇÃO PARA CONSULTAR STATUS DE RASTREAMENTO NO SITE DA LATAM
+def captura_status(awb):
+    driver.get(f"https://www.latamcargo.com/en/trackshipment?docNumber={awb}&docPrefix=957&soType=SO")
     
-    #resultado = f'A última consulta de rastreamento retornou: | AWB: {awb} | STATUS:{status} | DATA_EVENTO: {data} |'
-    #return render_template('index.html', resultado=resultado)
-    resultado = "Todas as consultas foram realizadas com sucesso"
-    return render_template('index.html', resultado=resultado, pendentes=lista)
+    wait = WebDriverWait(driver, 10)
+
+    status_evento = wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[1]')))
+    status = status_evento.text
+
+    data_evento = wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[6]')))
+    data = data_evento.text
+
+    captura = f'dados capturados: | AWB: {awb} | STATUS:{status} | DATA_EVENTO: {data} |'
+    print(captura)
+    print("="*150)
+    
+    return status, data, captura
+
+def capturar_status_pendentes():
+    dados_rastreamento = []
+    statuses = []  # Lista para armazenar os valores de status
+    datas = []  # Lista para armazenar os valores de data
+    capturas = []  # Lista para armazenar os valores de captura
+    
+    for awb in lista_pendentes:
+        status, data, captura = captura_status(awb)
+        dados_rastreamento.append({'AWB': awb, 'STATUS': status, 'DATA_EVENTO': data, 'CAPTURAS': captura})
+        statuses.append(status)  # Adicionar o status à lista
+        datas.append(data)  # Adicionar a data à lista
+        capturas.append(captura)  # Adicionar a captura à lista
+    
+    df_rastreamento = pd.DataFrame(dados_rastreamento)
+    
+    return df_rastreamento
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
