079748fb8f26e5268ac20ce75f2567bb3227ab1a
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-10-10 09:51:38 -0300

    a

--- DataFrame_No_Html/main.py
+++ DataFrame_No_Html/main.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def resultado():
     df = criar_dataframe()  # Chama a função para criar o DataFrame
+    print(df)
     table_html = df.to_html(classes='table table-bordered', index=False)
+    print(table_html)
+
     return render_template('resultado.html', table_html=table_html)
 
 if __name__ == '__main__':
