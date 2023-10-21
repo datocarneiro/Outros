bf96fdedd0908226bdcb51ba6daf8d12266fbde8
datocarneiro <datocarneiro@outlook.com>
2023-10-21 14:57:37 -0300

    Login validação simples

--- Validação de Login Simples/app.py
+++ Validação de Login Simples/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-senha_correta = "dato123"  # Senha correta
 nome_correto = "Dato"
+senha_correta = "dato123"  # Senha correta
 
 @app.route('/')
 def login():
