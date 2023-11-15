97be0cc54b77af8d6a6f83af12a250ad74d9f19d
datocarneiro <datocarneiro@outlook.com>
2023-11-15 19:19:52 -0300

    calculo investimento

--- Calculo_Investimento/app.py
+++ Calculo_Investimento/app.py
@@ -1,33 +1,17 @@
+investimento = float(input('qual é o valor do investimento? '))
+meses = int(input('Por quantos meses? '))
 
-# Solicita valor do investimento inicial
-investimento = float(input("Qual o valor do investimento? "))
-# Solicita ao usuário o número de meses a serem investidos e o valor do investimento inicial
-meses = int(input("Quantos meses será investido? "))
-rendimento = 0.01  # Taxa de rendimento mensal de 1%
-imposto = 0.15     # Taxa de imposto sobre os rendimentos apenas de 15%
-valorinvestido = investimento  # Salva o valor inicial do investimento
+imposto = 0.15
+rendimentos = 0.01
 
-print(f"\n", valorinvestido)
-
-# Loop que simula o investimento ao longo dos meses
 for i in range(0, meses):
-    print('\n-----------------------------------')
-    # Salva o valor inicial do investimento no início do mês
-    valor_inicial = investimento  
-    # Calcula o novo saldo após o rendimento mensal
-    investimento += investimento * rendimento  
-    print(f"SaldoMensal: {investimento}")
-    # Calcula o lucro mensal
-    lucro_mes = (investimento - valor_inicial)  
-
-# Calcula o lucro total ao final do período de investimento
-lucrototal = investimento - valorinvestido
-print(f"Lucro total {lucrototal}")
-
-# Calcula o desconto de imposto sobre o lucro total
-descontoImposto = lucrototal * imposto
-print(f"Desconto de imposto: {descontoImposto}")
-
-# Calcula o saldo final após o desconto de imposto
-saldoFinal = investimento - descontoImposto
-print(f"Saldo final: {saldoFinal}")
+    lucroreal = 0
+    valor_inicial = investimento ++ lucroreal
+    investimento +=  investimento * rendimentos
+    rendeu = investimento - valor_inicial
+    descontoimposto = rendeu * imposto
+    lucroreal = rendeu - descontoimposto
+    saldo = investimento - descontoimposto
+    investimento = investimento - descontoimposto
+    print(f'Saldo mensal: R$ {investimento}')
+    print("-"*50)
