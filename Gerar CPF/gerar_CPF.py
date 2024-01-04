026dbb00878fc18dc3fdeb2c9f0aa37fad2add9d
datocarneiro <datocarneiro@outlook.com>
2024-01-04 17:30:44 -0300

    implementação

--- Gerar CPF/gerar_CPF.py
+++ Gerar CPF/gerar_CPF.py
@@ -1,6 +1,5 @@
 while True:   
     while True:
-        print('='*50)
         try:
             cpf_input = input("Digite 9 digitos [apenas números]: ")
             # Verifica se a entrada contém apenas números
@@ -9,8 +8,7 @@ while True:
             informado = tuple(map(str, cpf_input))              
             # Verifica se o CPF tem 11 dígitos
             if len(informado) != 9:
-                raise ValueError('informe somente 9 dígitos.')
-            print('='*50)            
+                raise ValueError('informe somente 9 dígitos.')         
             break
         except ValueError as e:
             print(e)
@@ -22,7 +20,7 @@ while True:
         resultado += int(i) * numero_regressivo1
         numero_regressivo1 -= 1
     primeiro_digito = (resultado * 10) % 11 if (resultado * 10) % 11 <= 9 else 0
-    print(f'Validando o primeiro dígito:... {primeiro_digito}')
+    print(f'Gerando o primeiro dígito:... {primeiro_digito}')
     #concatenando os 9 digitos com o primeiro dígito
     dez_digitos = list(nove_digitos)
     dez_digitos.append(str(primeiro_digito))
@@ -33,18 +31,17 @@ while True:
         resultado += int(i) * numero_regressivo2
         numero_regressivo2 -= 1
     segundo_digito = (resultado * 10) % 11 if (resultado * 10) % 11 <= 9 else 0
-    print(f'Validando o segundo dígito:... {segundo_digito}')
+    print(f'Gerando o segundo dígito:... {segundo_digito}')
     #concatenando os 10 digitos com o segundo dígito
     resultado_cpf = dez_digitos
     resultado_cpf.append(str(segundo_digito)) 
     # formatando exibição
     cpf_unido = ''.join(resultado_cpf) # o delimitador é chamado antes
     cpf_validado = f'{cpf_unido[:9]}-{cpf_unido[9:]}'
-    print(f'\n\t{cpf_validado}\n')
+    print(f'\n\t cpf gerado: {cpf_validado}\n')
     continuar = input('Gerar outro? [s]im  [n]ão: ')
     if 's' in continuar:
         continue
     else:
         print('Você encerrou o programa\n')
-        print('='*50)
         break
