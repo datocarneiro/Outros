bebe7a0f76790d39869a9a623c6b273510ffb899
datocarneiro <datocarneiro@outlook.com>
2024-01-04 17:02:42 -0300

    implementação

--- Gerar CPF/gerar_CPF.py
+++ Gerar CPF/gerar_CPF.py
@@ -1,27 +1,3 @@
-
-# Calculo do primeiro dígito do CPF
-#     CPF: 746.824.890-70
-#     Colete a soma dos 9 primeiros dígitos do CPF
-#     multiplicando cada um dos valores por uma
-#     contagem regressiva começando de 10
-
-#     Ex.:  746.824.890-70 (746824890)
-#                         10  9  8  7  6  5  4  3  2
-#                       *  7  4  6  8  2  4  8  9  0
-#     Resultado da conta: 70  36 48 56 12 20 32 27 0
-
-#     Somar todos os resultados: 
-#     70+36+48+56+12+20+32+27+0 = 301
-#     Multiplicar o resultado anterior por 10
-#     301 * 10 = 3010
-#     Obter o resto da divisão da conta anterior por 11
-#     3010 % 11 = 7
-#     Se o resultado anterior for maior que 9:
-#         resultado é 0
-#     contrário disso:
-#         resultado é o valor da conta
-
-#     O primeiro dígito do CPF é 7
 while True:   
     while True:
         print('='*50)
@@ -44,6 +20,7 @@ while True:
 
     nove_digitos = informado
 
+    #calculando o primeiro dígito
     resultado = 0
     numero_regressivo1 = 10
     for i in nove_digitos:
@@ -53,37 +30,11 @@ while True:
     primeiro_digito = (resultado * 10) % 11 if (resultado * 10) % 11 <= 9 else 0
     print(f'Validando o primeiro dígito:... {primeiro_digito}')
 
-    #############################################################################################################
-
-    # Calculo do segundo dígito do CPF
-    # CPF: 746.824.890-70
-    # Colete a soma dos 9 primeiros dígitos do CPF,
-    # MAIS O PRIMEIRO DIGITO,
-    # multiplicando cada um dos valores por uma
-    # contagem regressiva começando de 11
-
-    # Ex.:  746.824.890-70 (7468248907)
-    #    11 10  9  8  7  6  5  4  3  2
-    # *  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
-    #    77 40 54 64 14 24 40 36  0 14
-
-    # Somar todos os resultados:
-    # 77+40+54+64+14+24+40+36+0+14 = 363
-    # Multiplicar o resultado anterior por 10
-    # 363 * 10 = 3630
-    # Obter o resto da divisão da conta anterior por 11
-    # 3630 % 11 = 0
-    # Se o resultado anterior for maior que 9:
-    #     resultado é 0
-    # contrário disso:
-    #     resultado é o valor da conta
-
-    # O segundo dígito do CPF é 0
-
     #concatenando os 9 digitos + o primeiro dígito
     dez_digitos = list(nove_digitos)
     dez_digitos.append(str(primeiro_digito))
 
+    #calculando o segundo dígito
     resultado = 0
     numero_regressivo2 = 11
     for i in dez_digitos:
@@ -95,8 +46,7 @@ while True:
 
     resultado_cpf = dez_digitos
 
-    resultado_cpf.append(str(segundo_digito))
- 
+    resultado_cpf.append(str(segundo_digito)) 
 
     cpf_unido = ''.join(resultado_cpf) # o delimitador é chamado antes
 
